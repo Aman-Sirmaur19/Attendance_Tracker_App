9ebeac79ff88c5cb71d42e846d3d016ad568ace6
Aman-Sirmaur19 <amansirmaur190402@gmail.com>
2024-12-04 06:38:17 +0530

    Updated UI.

--- lib/widgets/task_widget.dart
+++ lib/widgets/task_widget.dart
@@ -22,72 +22,78 @@ class _TaskWidgetState extends State<TaskWidget> {
   Widget build(BuildContext context) {
     return Card(
       color: widget.task.isCompleted ? Colors.blue.shade100 : null,
-      child: ListTile(
+      child: GestureDetector(
         onTap: () => Navigator.push(
             context,
             CupertinoPageRoute(
                 builder: (_) => AddTaskScreen(task: widget.task))),
-        leading: IconButton(
-          icon: widget.task.isCompleted
-              ? const Icon(Icons.check_circle, color: Colors.green)
-              : const Icon(Icons.circle_outlined),
-          onPressed: () => setState(() {
-            widget.task.isCompleted = !widget.task.isCompleted;
-            widget.task.save();
-          }),
-        ),
-        title: Text(
-          widget.task.title,
-          style: TextStyle(
-              color: widget.task.isCompleted ? Colors.blue : null,
-              fontSize: 15,
-              letterSpacing: 1,
-              fontWeight: FontWeight.bold,
-              decoration:
-                  widget.task.isCompleted ? TextDecoration.lineThrough : null),
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.task.subTitle,
-              style: TextStyle(
-                  fontSize: 15,
-                  color: widget.task.isCompleted
-                      ? Colors.blue.shade300
-                      : Colors.black45,
-                  fontWeight: FontWeight.bold,
-                  decoration: widget.task.isCompleted
-                      ? TextDecoration.lineThrough
-                      : null),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    DateFormat.yMMMEd().format(widget.task.createdAtDate),
-                    style: TextStyle(
-                      fontSize: 13,
-                      color:
-                          widget.task.isCompleted ? Colors.white : Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    widget.task.createdAtTime,
-                    style: TextStyle(
-                      fontSize: 13,
-                      color:
-                          widget.task.isCompleted ? Colors.white : Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+        child: ListTile(
+          leading: IconButton(
+            onPressed: () => setState(() {
+              widget.task.isCompleted = !widget.task.isCompleted;
+              widget.task.save();
+            }),
+            tooltip: widget.task.isCompleted ? 'Deselect' : 'Select',
+            icon: widget.task.isCompleted
+                ? const Icon(Icons.check_circle, color: Colors.green)
+                : const Icon(Icons.circle_outlined),
+          ),
+          title: Text(
+            widget.task.title,
+            style: TextStyle(
+                color: widget.task.isCompleted ? Colors.blue : null,
+                fontSize: 15,
+                letterSpacing: 1,
+                fontWeight: FontWeight.bold,
+                decoration: widget.task.isCompleted
+                    ? TextDecoration.lineThrough
+                    : null),
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.task.subTitle,
+                style: TextStyle(
+                    fontSize: 15,
+                    color: widget.task.isCompleted
+                        ? Colors.blue.shade300
+                        : Colors.black45,
+                    fontWeight: FontWeight.bold,
+                    decoration: widget.task.isCompleted
+                        ? TextDecoration.lineThrough
+                        : null),
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.centerRight,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      DateFormat.yMMMEd().format(widget.task.createdAtDate),
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: widget.task.isCompleted
+                            ? Colors.white
+                            : Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      widget.task.createdAtTime,
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: widget.task.isCompleted
+                            ? Colors.white
+                            : Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
